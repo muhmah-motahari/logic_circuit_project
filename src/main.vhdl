ce96a3d554fdd2d59c486af2e60a144e4aedfc13
Muhammad Mahdi Motahari <mm.motahari.2001@gmail.com>
2022-01-31 13:09:41 +0330

    some updates but not complete

--- src/main.vhdl
+++ src/main.vhdl
@@ -11,13 +11,22 @@ architecture struct_main of main is
 		port(d, clk : in std_logic;
 			q : out std_logic);
 	end component;
+    component multiplexer_2x1 is
+        port(w0, w1, s : in std_logic;
+            output : out std_logic);
+    end component;
+
 	signal notSO, notSC : std_logic;
-    signal s1, s2 : std_logic;
+    signal s1, s2, s3, s4 : std_logic;
+    signal one : std_logic := '1';
+    signal zero : std_logic := '0';
 begin
 	notSO <= not so;
     notSC <= not sc;
 	flp0: flipflop port map(clk => input, d => sc, q => s1);
     flp1: flipflop port map(clk => input, d => so, q => s2);
-    EOpening <= notSO and s1;
-    EClosing <= notSC and s2;
+    mul0: multiplexer_2x1 port map(w0 => s1, w1 => one, s => sb, output => s3);
+    mul1: multiplexer_2x1 port map(w0 => s2, w1 => zero, s => sb, output => s4);
+    EOpening <= notSO and s3;
+    EClosing <= notSC and s4;
 end architecture;
--- src/testbench.vhdl
+++ src/testbench.vhdl
@@ -12,7 +12,8 @@ architecture struct_testbench of testbench is
 	signal input, sc, so, sb, EOpening, EClosing : std_logic;
 begin
 	uut: main port map(input, sc, so, sb, EOpening, EClosing);
-    input <= '0', '1' after 0.8 ns, '0' after 3 ns, '1' after 9 ns, '0' after 11 ns;
+    input <= '0', '1' after 0.8 ns, '0' after 3 ns, '1' after 9 ns, '0' after 11 ns, '1' after 14 ns, '0' after 14.5 ns, '1' after 16 ns;
     sc <= '1', '0' after 0.89 ns, '1' after 17 ns;
     so <= '0', '1' after 7 ns, '0' after 9.09 ns;
+    sb <= '0', '1' after 12 ns, '0' after 15 ns;
 end architecture;
